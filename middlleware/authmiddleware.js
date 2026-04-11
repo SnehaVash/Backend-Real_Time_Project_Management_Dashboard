bddb42d169585db94e9d1520c5c96f0e293a479e
Srijan0405 <srijanhardik16@gmail.com>
2026-04-12 00:14:01 +0530

    [Srijan] Updated auth middleware

--- middlleware/authmiddleware.js
+++ middlleware/authmiddleware.js
@@ -1,23 +1,49 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+export const verifyToken = async (req, res, next) => {
+  const header = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).send("Token not provided");
+  if (!header) {
+    return res.status(401).send("No token provided");
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = header.split(" ")[1];
 
   if (!token) {
     return res.status(401).send("Invalid token");
   }
 
   try {
-    const decoded = jwt.verify(token, "secretkey");
-    req.user = decoded; // store user info for later use
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).send("User not found");
+    }
+
+    if (!user.isActive) {
+      return res.status(403).send("Account is deactivated");
+    }
+
+    req.user = user;
+
     next();
+
   } catch (err) {
     return res.status(401).send("Token is not valid");
   }
+};
+
+export const checkRole = (...roles) => {
+  return (req, res, next) => {
+    const userRole = req.user?.role;
+
+    if (!userRole || !roles.includes(userRole)) {
+      return res.status(403).send("Access denied");
+    }
+
+    next();
+  };
 };
