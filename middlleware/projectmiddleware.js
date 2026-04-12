e4574c4e970794ca018a28910f02e237a52ac94c
Srijan0405 <srijanhardik16@gmail.com>
2026-04-12 19:46:09 +0530

    [Srijan] Updated Middleware

---
+++ middlleware/projectmiddleware.js
@@ -0,0 +1,52 @@
+import Project from "../models/project.model.js";
+
+export const isProjectOwner = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Project.findById(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    if (project.createdBy.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    req.project = project;
+
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid project id" });
+  }
+};
+
+
+export const isProjectMemberOrOwner = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Project.findById(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    const isOwner = project.createdBy.toString() === req.user._id.toString();
+
+    const isMember = project.members.some(
+      m => m.toString() === req.user._id.toString()
+    );
+
+    if (!isOwner && !isMember) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    req.project = project;
+
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid project id" });
+  }
+};
