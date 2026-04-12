e72193b85b48faccbb74f6edb69bdaedb109bd1d
Sneha Vashishtha <snehavashishtha38@gmail.com>
2026-04-12 12:50:54 +0530

    [Sneha] Added app.js entry configuration

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import activityLogRoutes from "./routes/activityLogRoutes.js";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/logs", activityLogRoutes);
+
+app.use(errorMiddleware);
+
+export default app;
