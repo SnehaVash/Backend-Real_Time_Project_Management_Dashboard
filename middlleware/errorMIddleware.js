2663b6dea1bc6226ca38157e534465b8dcf6ae95
Srijan0405 <srijanhardik16@gmail.com>
2026-04-14 21:30:58 +0530

    Delete middlleware directory

--- middlleware/errorMIddleware.js
+++
@@ -1,7 +0,0 @@
-export default function errorMiddleware(err, req, res, next) {
-  const status = err.status || 500;
-
-  res.status(status).json({
-    message: err.message || "Server error"
-  });
-}
